8f361d66aef018e559efe273b624bdc1bd52207e
stekat <stefandietsch@gmail.com>
2019-01-11 09:46:31 +0100

    Aktuelle Seite deaktiviert

--- index_htm_files/slideshowTop_config.js
+++
@@ -1,9 +0,0 @@
-function xaraSwidgets_slideshowTopConstructor(divID, data)
-{
-	xaraSwidgets_slideshowTemplateConstructor(divID, data, {
-		
-		orientation:'top',
-		originalWidth:600,
-		originalHeight:470
-	});
-}
